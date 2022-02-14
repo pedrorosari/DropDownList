f85e46b0e921f41caf63d5c11ef7a33e70730199
Shurumela com estilo <pedro.maria@bandtec.com.br>
2022-02-14 12:15:40 -0300

    Projeto

---
+++ DropdownList/Controllers/DepartmentsController.cs
@@ -0,0 +1,24 @@
+﻿using DropdownList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace DropdownList.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index(Department department)
+        {
+            Department contrato = new Department();
+
+
+
+            List < Department > list = new List<Department>();
+                list.AddRange(contrato.dirs);
+
+
+
+            return View(list);
+        }
+    }
+}
---
+++ DropdownList/Models/Department.cs
@@ -0,0 +1,31 @@
+﻿namespace DropdownList.Models
+{
+    public class Department
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public string [] dirs
+        {
+            get { return dirs; }
+           
+           
+}
+
+        public void Contratos()
+        {
+            Contratos(dirs);
+        }
+
+        public void Contratos(string[] dirs)
+        {
+            dirs = Directory.GetDirectories(@"c:\", "*", SearchOption.TopDirectoryOnly);
+
+         
+
+        }
+      
+        
+       
+}
+}
