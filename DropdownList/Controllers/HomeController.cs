831dddb8430cd85c7ede98d594d1b2671a503c8c
Shurumela com estilo <pedro.maria@bandtec.com.br>
2022-02-13 21:07:10 -0300

    Primeiros Passos

---
+++ DropdownList/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using DropdownList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DropdownList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["Email"] = "pedro.maria@badntec.com.br";
+            ViewData["Numero"] = "11 963817208";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
